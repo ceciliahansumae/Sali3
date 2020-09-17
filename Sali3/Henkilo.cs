84eb476258bb5918c8a70c19590f682f5f98480e
ceciliahansumae <70315702+ceciliahansumae@users.noreply.github.com>
2020-09-17 01:08:37 -0700

    Lisätty luokkia

--- Sali3/Henkilo.cs
+++ Sali3/Henkilo.cs
@@ -7,6 +7,7 @@ namespace Sali3
     class Henkilo
     {
         // Määritellään kaikkien henkilöiden yhteiset ominaisuudet (kentät)
+        //Jäsen-luokka perii nämä ominaisuudet
         protected string etunimi;
         protected string sukunimi;
         protected string katuosoite;
@@ -23,7 +24,7 @@ namespace Sali3
             this.postitoimipaikka = "N/A";
         }
         
-        // Muodostin kaikilla parametreilla
+        // Muodostin kaikilla parametreillä
 
         public Henkilo(string etu, string suku, string katu, string pnro, string paikka)
         {
@@ -32,6 +33,44 @@ namespace Sali3
             this.katuosoite = katu;
             this.postinumero = pnro;
             this.postitoimipaikka = paikka;
+        }
+
+        // Metodeja testaukseen 
+        public void naytaHenkilo()
+        {
+            Console.WriteLine("Etunimi on " + this.etunimi);
+            Console.WriteLine("Sukunimi on " + this.sukunimi);
+            Console.WriteLine("Katuosoite on " + katuosoite);
+            Console.WriteLine("Postinumero on " + this.postinumero);
+            Console.WriteLine("Postitoimipaikka on " + this.postitoimipaikka);
+        }
+    }
+    class Jasen : Henkilo
+    {
+        // Jäsen-olioiden erityisominaisuudet, joita ei ole Henkilo-luokassa
+        protected string jasenid;
+        protected string sukupuoli;
+
+        // Muodostin kaikilla parametreillä
+        public Jasen(string etu, string suku, string katu, string pnro, string paikka, string id, string sukup)
+        {
+            this.etunimi = etu;
+            this.sukunimi = suku;
+            this.katuosoite = katu;
+            this.postinumero = pnro;
+            this.postitoimipaikka = paikka;
+            this.jasenid = id;
+            this.sukupuoli = sukup;
+        }
+        public void naytaJasen()
+        {
+            Console.WriteLine("Etunimi on " + this.etunimi);
+            Console.WriteLine("Sukunimi on " + this.sukunimi);
+            Console.WriteLine("Katuosoite on " + katuosoite);
+            Console.WriteLine("Postinumero on " + this.postinumero);
+            Console.WriteLine("Postitoimipaikka on " + this.postitoimipaikka);
+            Console.WriteLine("Jäsennumero on " + this.jasenid);
+            Console.WriteLine("Sukupuoli on " + this.sukupuoli);
         }
     }
 }
--- Sali3/Program.cs
+++ Sali3/Program.cs
@@ -9,9 +9,40 @@ namespace Sali3
         {
             Console.WriteLine("Testataan Henkilo-olion luontia");
 
+            /*---------------------
+             | TESTAUS ALKAA TÄSTÄ |
+             ----------------------*/
+
             // Testataan oletusmuodostin
 
             Henkilo henkilo = new Henkilo();
+
+            // Testataan muodostin kaikilla parametreillä
+
+            Henkilo henkilo1 = new Henkilo("Mika", "Hakala", "Kotikatu 3", "20100", "Turku");
+
+            // Testataan Jäsen-olion muodostimen toiminta
+            Jasen jasen = new Jasen("Mika", "Vainio", "Yläneentie 146", "23100", "Mynämäki", "123", "1");
+
+            // Testataan Trainer-olion muodostimen toiminta, huom. nimiavaruus Henkilosto
+            Henkilosto.Trainer trainer = new Henkilosto.Trainer("Jakke", "Jäynä", "Kotikatu 2", "21200", "Raisio", "Juoksumatto", "044 7057425", "jakke@huuhaa.com");
+
+            // Testatataan Punnitus-olion toimintaa
+
+            Punnitus punnitus = new Punnitus("10.9.2020", "70,5", "1,71");
+
+            // Katsotaan olioiden ominaisuuksia
+            Console.WriteLine("Testi henkilön oletusmuodostimelle");
+            henkilo.naytaHenkilo();
+            Console.WriteLine("Testi henkilön muodostimelle kaikilla parametreilla");
+            henkilo1.naytaHenkilo();
+            Console.WriteLine("Testi jäsenen muodostimelle");
+            jasen.naytaJasen();
+            Console.WriteLine("Testi personal trainerin muodostimelle");
+            trainer.naytaTrainer();
+            Console.WriteLine("Testi punnituksen muodostimelle");
+            punnitus.naytaPunnitus();
+            Console.WriteLine("Painoindeksi (BMI-metodi) on " + punnitus.BMI());
         }
     }
 }
